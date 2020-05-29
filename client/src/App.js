16fef71e0242da5bcc2b41af0891400a1601e2d7
Nickds54 <nicksalter98@gmail.com>
2020-05-29 12:53:56 -0500

    Added assets and importing antd.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Route, Switch,BrowserRouter as Router } from 'react-router-dom';
-import Home from './Pages/Home'
-import './App.css';
+import Home from './Pages/Home';
+import MyHeader from './Components/header';
+import {Layout} from 'antd';
+import 'antd/dist/antd.css';
+
+const {Header, Content} = Layout
 
 function App() {
   return (
    <>
     <Router>
-      <Switch>
-        <Route path="/"><Home/> </Route>
-      </Switch>
-      </Router>
+      <Layout className="layout">
+          <Header><MyHeader/></Header>
+          <Layout>
+        <Content>
+          <Switch>
+            <Route path="/"><Home/></Route>
+          </Switch>
+        </Content>
+        </Layout>
+      </Layout>
+    </Router>
    </>
   );
 }
