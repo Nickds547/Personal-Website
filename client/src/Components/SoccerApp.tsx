00da93001b3b7be91e4e30e6978a3bb36589d448
Nickds54 <nicksalter98@gmail.com>
2020-07-03 14:33:08 -0500

    Added features and more screenshots to soccer scheduling app

--- client/src/Components/SoccerApp.tsx
+++ client/src/Components/SoccerApp.tsx
@@ -2,28 +2,106 @@ import React from 'react';
 import styled from 'styled-components'
 import calendar from '../Assets/SoccerAppPictures/calendar.svg';
 import users from '../Assets/SoccerAppPictures/users.svg';
+import gm from '../Assets/SoccerAppPictures/gameManager.svg';
 import {Button} from 'antd';
 import {MyTitle} from '../Styles/HomePage';
 
 const Wrapper = styled.div`
     display: grid;
-    grid-template-columns: 10% 15% 20% auto 15% 10%;
+    grid-template-columns: 16.6% 16.6% 16.6% 16.6% 16.6% auto;
     grid-template-rows: 15% 25% 50% auto;
     height: 100%;
     padding-top: 2vh;
+    font-size: 18pt;
+    font-weight: bold;
 `;
 
 const Cal = styled.img`
-
+    grid-column: 2 / span 3;
+    grid-row: 2 / span 2;
+    justify-self: center;
+    place-self: stretch;
 `;
+
+const Description = styled.div`
+    grid-column: 2 / span 3;
+    grid-row: 4 / span 1;
+    justify-self: center;
+`;
+
+const Features = styled.div`
+    grid-column: 5 / span 1;
+    grid-row: 2/ span 2;
+    justify-self: center;
+`;
+
+const SubTitle = styled.span`
+   text-decoration: underline;
+   font-weight: bolder;
+   margin-left: 10%;
+`;
+
+const Title1 = styled.div`
+    grid-column: 1 / span 2;
+    grid-row: 1 / span 1;
+    justify-self: center;
+    align-self: end;
+`;
+const Pic1 = styled.img`
+    grid-column: 1 / span 2;
+    grid-row: 2 / span 3;
+    place-self: stretch;
+    justify-self: center;
+    width: 90%;
+`;
+
+const Title2 = styled.div`
+    grid-column: 3 / span 2;
+    grid-row: 1 / span 1;
+    justify-self: center;
+    align-self: end;
+`;
+const Pic2 = styled.img`
+    grid-column: 3 / span 2;
+    grid-row: 2 / span 3;
+    place-self: stretch;
+    justify-self: center;
+    width: 90%;
+`;
+
 
 const SoccerApp = ({setProject}: any) =>{
     return(
         <>
         <Wrapper>
             <MyTitle>Soccer Scheduling App</MyTitle>
+            <Cal src={calendar}/>
+            <Description>
+                The Soccer Scheduling app was built for ULM's software engineering class as a tool to assist coaches in North East Louisiana. It's main function 
+                was to automate the process of accepting game details and finding schedule conflicts. 
+            </Description>
+            <Features>
+                <SubTitle>Included Features:</SubTitle>
+                <ul>
+                    <li>Dynamic form for adding games</li>
+                    <li>Master schedule showing all scheduled games</li>
+                    <li>Multiple calendar filters</li>
+                    <li>Email notifications</li>
+                    <li>Admin ability to block days</li>
+                    <li>Arbiter Export for use in third party application</li>
+                    <li>Game Manager for seeing list of all relevant games</li>
+                    <li>Add Games through spreadsheet</li>
+                </ul>
+            </Features>
         </Wrapper>
-        <Button onClick={()=>setProject("home")}>Go Back</Button>
+        <Wrapper style={{marginTop: "5%"}}>
+            <Title1>User Controller</Title1>
+            <Pic1 src={users}/>
+
+            <Title2>Game Manager</Title2>
+            <Pic2 src={gm}/>
+        </Wrapper>
+        <Button style={{marginLeft: "45%", marginTop: "5%"}} onClick={()=>setProject("home")}>Go Back</Button>
         </>
     )
 }
